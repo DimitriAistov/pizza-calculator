26552cc4b2da30241ea210b30ff42840927ca660
DimitriAistov <54978052+DimitriAistov@users.noreply.github.com>
2019-09-18 08:50:24 +0200

    Add files via upload

---
+++ Pizza calculator/pizza calculator.js
@@ -0,0 +1,21 @@
+var small = prompt("Hoeveel small pizzas's wilt U? €4");
+document.write("Small pizza's:" + '' + small + '<br></br>');
+
+
+
+var medium = prompt("Hoeveel medium pizzas's wilt U? €6");
+document.write("Medium pizza's:" + '' + medium + '<br></br>');
+
+
+var large = prompt("Hoeveel large pizzas's wilt U? €8");
+document.write("Large pizza's:" + '' + large + '<br></br>');
+
+
+var Small = 4.00;
+var Medium = 6.00;
+var Large = 8.00;
+var price = small * Small + medium * Medium + Large * large
+document.write('Totale prijs: €' + price)
+
+
+
